255dc061e86f25b26e5ab07b4ca773b2606b8aa3
Julia Grabow <grajoh@protonmail.com>
2025-07-29 17:09:57 -0500

    JG: cleaned and added 30 day moving geometric mean

--- MSP-beaches_Minnetonka.R
+++ MSP-beaches_Minnetonka.R
@@ -109,7 +109,19 @@ minnetonka <- minnetonka_raw # create df to manipulate
 str(minnetonka)
 summary(minnetonka)
 
-# Change values that are "<1" to 0
+# Change values that are "<1" to 1----
+
+Expected <-
+  length(which(minnetonka$sample1 == "<1")) +
+  length(which(minnetonka$sample1 == "1")) +
+  length(which(minnetonka$sample2 == "<1")) +
+  length(which(minnetonka$sample2 == "1")) +
+  length(which(minnetonka$sample3 == "<1")) +
+  length(which(minnetonka$sample3 == "1")) +
+  length(which(minnetonka$sample4 == "<1")) +
+  length(which(minnetonka$sample4 == "1")) +
+  length(which(minnetonka$sample5 == "<1")) +
+  length(which(minnetonka$sample5 == "1"))
 
 minnetonka <-
   minnetonka %>%
@@ -119,10 +131,17 @@ minnetonka <-
       .fns = ~ str_replace(
         string = .x,
         pattern = "<1",
-        replacement = "0"
+        replacement = "1"
       )
     )
   )
+
+length(which(minnetonka$sample1 == "1")) +
+  length(which(minnetonka$sample2 == "1")) +
+  length(which(minnetonka$sample3 == "1")) +
+  length(which(minnetonka$sample4 == "1")) +
+  length(which(minnetonka$sample5 == "1")) == Expected # True
+
 
 # Convert numeric rows from character to numeric----
 
@@ -130,7 +149,7 @@ minnetonka <-
   minnetonka %>%
   mutate(
     across(
-      .cols = 2:10,
+      .cols = 2:7,
       .fns = as.numeric
     )
   )
@@ -156,70 +175,6 @@ minnetonka <-
   minnetonka %>%
   drop_na(
     "sample1"
-  )
-
-# Denote when beaches were closed----
-## Look at values in non-numeric cols
-
-str(minnetonka)
-unique(minnetonka$Notes)
-### retesting value = 6.3, possible contamination, CLOSED
-unique(minnetonka$Temporary1)
-### closed, CLOSED
-unique(minnetonka$Temporary2)
-### No notes that need recording
-
-## Make changes to note beach closures and other notes----
-### create beach closure col
-
-minnetonka <-
-  minnetonka %>%
-  mutate( # replace value with retested value
-    sample3 = if_else(
-      condition = sample3 == 200.5,
-      true = 6.3,
-      false = sample3
-    )
-  )
-
-x <- # create vector that identifies presence of closed in Notes col
-  str_detect(
-    string = minnetonka$Notes,
-    pattern = "CLOSED"
-  )
-
-y <- # create vector that identifies presence of closed in Temporary1 col
-  str_detect(
-    string = minnetonka$Temporary1,
-    pattern = "CLOSED | closed"
-  )
-
-minnetonka <- # add x and y as new cols to minnetonka
-  minnetonka %>%
-  mutate(
-    Temporary3 = x,
-    Temporary4 = y
-  )
-
-minnetonka <- # combine x and y cols into ClosureYN
-  minnetonka %>%
-  unite(
-    col = "ClosureYN",
-    Temporary3, Temporary4,
-    sep = ""
-  )
-
-minnetonka <- # change true and false to Y and N
-  minnetonka %>%
-  mutate(
-    ClosureYN = if_else(
-      condition = str_detect(
-        string = ClosureYN,
-        pattern = "TRUE"
-      ),
-      true = "Y",
-      false = "N"
-    )
   )
 
 # To notes column, use language provided by BF for contamination and retesting
@@ -254,54 +209,6 @@ minnetonka <- # note that contaminated and retested data as suspicioius
     )
   )
 
-# Create missing cols----
-## cols with consistent values----
-
-minnetonka <-
-  minnetonka %>%
-  mutate(
-    Ecoli_30dGM = NA,
-    Ecoli_units = "cfu",
-    Entero_avg_cfu = NA,
-    Microcystin_ugL = NA,
-    Cylindro_ugL = NA,
-    Anatoxin_ugL = NA,
-    ClosureReason = NA,
-    MonitoringOrg = "Minnetonka"
-  )
-
-## cols with values based on another col
-## DNRID for shady oak = 27-0089
-## DNRID for libbs lake = 27-0085
-
-minnetonka <- # add DNRID for shady oak
-  minnetonka %>%
-  mutate(
-    DNRID = if_else(
-      condition = BeachName == "Shady Oak",
-      true = "27-0089",
-      false = NA
-    )
-  )
-
-minnetonka <- # add DNRID for shady oak
-  minnetonka %>%
-  mutate(
-    DNRID = if_else(
-      condition = BeachName == "Libbs Lake",
-      true = "27-0085",
-      false = DNRID
-    )
-  )
-
-# Rename temp col----
-
-minnetonka <-
-  minnetonka %>%
-  rename(
-    Temp_F = "Ambient Water Temp. oF"
-  )
-
 # Remove unneeded cols (clearing clutter)
 
 minnetonka <-
@@ -310,13 +217,13 @@ minnetonka <-
     !c(
       "Composite E. coli (CFU/100ml)",
       "5-Sample Geo. Mean E. coli (CFU/100ml)",
+      "Ambient Water Temp. oF",
       Temporary3,
       Temporary1
     )
   )
 
-# Calculate geometric means----
-## playing--how to calculate mean in current form of df
+# Calculate geometric mean for sampling date----
 ## pivot longer----
 
 Expected <-
@@ -332,33 +239,226 @@ minnetonka <-
 
 nrow(minnetonka) == Expected # True
 
-## calculate geometric mean----
+## calculate product for each sampling date per site----
 
 minnetonka <-
   minnetonka %>%
   mutate(
-    Ecoli_1dGM = exp(mean(log(Meas[Meas>0]))),
-    .by = c(BeachName, Date),
-    .after = Date
-  ) %>%
+    product = prod(Meas),
+    .by = c(BeachName, Date)
+  )
+
+## determine how many sample size for each date per site----
+
+minnetonka <-
+  minnetonka %>%
+  mutate(
+    n = n(),
+    .by = c(BeachName, Date)
+  )
+
+## find the nth log of the products----
+### create a vector for the base for the log transformation-----
+
+v <- as.numeric(unique(minnetonka$n))
+
+### find the nth log
+
+minnetonka <-
+  minnetonka %>%
+  mutate(
+    Ecoli_1dGM = log(
+      x = product,
+      base = v
+    ),
+    .by = c(BeachName, Date)
+  )
+
+# Find 30 day geometric mean----
+## create separate df for 30 day calculations----
+
+x <- minnetonka
+
+## keep only needed cols----
+
+x <-
+  x %>%
   select(
-    !c(SampleID, Meas)
-  ) %>%
-  distinct() 
+    BeachName, Date, SampleID, Meas
+  )
 
-# Note when beach closed due to high ecoli levels----
+## create df with all time intervals for each site----
 
-minnetonka <-
-  minnetonka %>%
+y <-
+  x %>%
   mutate(
-    ClosureReason = if_else(
-      condition = Ecoli_1dGM > 126,
-      true = "Ecoli_1dGM",
-      false = ClosureReason
+    date_30d = interval(
+      start = Date - 30,
+      end = Date
     )
   )
 
-# Convert POSIXct to m/d/yyyy----
+y <-
+  y %>%
+  distinct(BeachName, Date, date_30d)
+
+## Cross join both df's----
+
+Expected <- nrow(x) * nrow(y)
+
+z <-
+  cross_join(
+    x, y
+  )
+
+nrow(z) == Expected # True
+
+## Keep only rows where beach name is the same and date within interval----
+
+z <-
+  z %>%
+  filter(
+    BeachName.x == BeachName.y
+  )
+
+## Keep only rows where Date is within date interval----
+
+z <-
+  z %>%
+  filter(
+    Date.x %within% date_30d
+  )
+
+## determine how many sample size for each date per site----
+
+z <-
+  z %>%
+  mutate(
+    n = n(),
+    .by = c("BeachName.x", "date_30d")
+  )
+
+## calculate product for each sampling date per site----
+
+z <-
+  z %>%
+  mutate(
+    product = prod(Meas),
+    .by = c("BeachName.x", "date_30d")
+  )
+
+## find the nth log of the products----
+### find the nth log
+
+z <-
+  z %>%
+  mutate(
+    Ecoli_30dGM = log(
+      x = product,
+      base = n
+    ),
+    .by = c("BeachName.x", "date_30d")
+  )
+
+## Remove rows where Date.x does not match with Date.y to reduce to one meas----
+### per date per site
+
+z <-
+  z %>%
+  filter(
+    Date.x == Date.y
+  )
+
+## keep only BeachName.x, Date.x, and Ecoli_30d----
+
+z <-
+  z %>%
+  select(
+    BeachName.x, Date.x, Ecoli_30dGM
+  ) %>%
+  unique() %>%
+  arrange(BeachName.x, Date.x)
+
+## Rename cols----
+
+z <-
+  z %>%
+  rename(
+    BeachName = BeachName.x,
+    Date = Date.x
+  )
+
+## add to minnetonka df----
+### drop unneeded cols from z and minnetonka dfs
+
+minnetonka <-
+  minnetonka %>%
+  select( # remove unneeded cols
+    !c(SampleID, Meas, product, n)
+  ) %>%
+  distinct() # keep only unique rows
+
+### merge both df's into one
+
+minnetonka <-
+  left_join(
+    x = minnetonka,
+    y = z,
+    by = c("BeachName", "Date")
+  )
+
+# Note when beach closed due to high ecoli levels----
+## are there days when 1 day gm > 1260?
+
+minnetonka$Ecoli_1dGM > 1260 # none over 1260
+
+# are there days when 30 day gm > 126
+
+minnetonka$Ecoli_30dGM > 126 # none over 126
+
+## Do not need to note when beach closed due to E. coli levels
+
+# Standardize df to match master df----
+## Add missing cols with single value----
+
+minnetonka <-
+  minnetonka %>%
+  mutate(
+    Ecoli_units = "cfu",
+    Entero_avg_cfu = NA,
+    Microcystin_ugL = NA,
+    Cylindro_ugL = NA,
+    Anatoxin_ugL = NA,
+    ClosureYN = "N",
+    ClosureReason = NA,
+    MonitoringOrg = "Minnetonka"
+  )
+
+## add cols with values based on another col----
+## DNRID for shady oak = 27008900
+## DNRID for libbs lake = 27008500
+
+minnetonka <- # add DNRID for shady oak
+  minnetonka %>%
+  mutate(
+    DNRID = if_else(
+      condition = BeachName == "Shady Oak",
+      true = 27008900,
+      false = NA
+    )
+  )
+
+minnetonka <- # add DNRID for Libbs Lake
+  minnetonka %>%
+  mutate(
+    DNRID = if_else(
+      condition = BeachName == "Libbs Lake",
+      true = 27008500,
+      false = DNRID
+    )
+  )
+
+## Convert POSIXct to m/d/yyyy----
 
 minnetonka <-
   minnetonka %>%
